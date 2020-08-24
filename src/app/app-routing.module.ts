8aa6619351c564eb060259f2b61a525b0ed416df
Nathan Pickard <nathan.pickard87@gmail.com>
2020-08-23 22:09:59 -0700

    Upgrade to Angular 10 and adding listings page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ListingsPageComponent } from './listings-page/listings-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'listings', component: ListingsPageComponent, pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
