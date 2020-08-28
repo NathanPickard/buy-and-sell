7950b60ec812bbe5ec10d94b5187972be67fd7e3
Nathan Pickard <nathan.pickard87@gmail.com>
2020-08-27 20:16:41 -0700

    New listing and my listing page forms and functions

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewListingPageComponent implements OnInit {
 
-  constructor() { }
+  name = '';
+  description = '';
+  price: '';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
   }
 
 }
